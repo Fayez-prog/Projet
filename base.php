0709f460655b876ecc024b4e10a2cee4c47168b6
Fayez Samet <73030296+Fayez-prog@users.noreply.github.com>
2021-07-13 19:15:19 +0100

    Add files via upload

--- base.php
+++ base.php
@@ -1,5 +1,5 @@
 <?php 
-$conn=mysqli_connect("remotemysql.com", "A28E1pBk8g", "dq27AU2Mfe","A28E1pBk8g"); 
+$conn=mysqli_connect("localhost", "root", "","esp"); 
 if (!$conn) {
 	echo "<p>Erreur de connexion a la base de donnees</p>"; 
 	exit();
