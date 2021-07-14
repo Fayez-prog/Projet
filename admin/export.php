dd240cef765a42b6d807ba427e39a311ed61dd52
Fayez Samet <73030296+Fayez-prog@users.noreply.github.com>
2021-07-14 07:25:52 +0100

    Add files via upload

--- admin/export.php
+++ admin/export.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 $csv = '';
 $csv_nom = 'fichier.csv';
-$mysqli = mysqli_connect("remotemysql.com", "A28E1pBk8g", "dq27AU2Mfe", "A28E1pBk8g");
+$mysqli = mysqli_connect("sql4.freesqldatabase.com", "sql4425068", "JS6dPCwvgA", "sql4425068");
 $result = mysqli_query($mysqli, "SELECT id_resultat, courant_bat, courant_pv, courant_ph, tension_bat, tension_pv, tension_ph, date, time FROM resultat");
 $champ = mysqli_num_fields($result);
 for($i = 0; $i < $champ; $i++) 
